e4ebe2562d8e9afe9599c6ca909d149faa9af272
Viespa <victorep105@gmail.com>
2023-10-23 23:04:34 +0200

    build: :wrench: Add Socket.io

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
 import express from "express";
 import logger from "morgan";
 
+import { Server } from "socket.io";
+import { createServer } from "node:http";
+
 const port = process.env.PORT ?? 3000;
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+    console.log("A user connected");
+    
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
+    });
+});
 
 app.use(logger('dev'));
 
@@ -11,6 +24,6 @@ app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/client/index.html');
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
